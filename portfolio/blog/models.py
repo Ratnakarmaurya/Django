99a6e7dd0747f3526f874015df43cfa2098b22fc
Ratnakar Maurya <mauryaratnakar@gmail.com>
2021-07-10 15:09:41 +0530

    blog created

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-# Create your models here.
-Class Job(models.Model):
-
-
+class Blog(models.Model):
+  title = models.CharField(max_length=20)
+  publication_date = models.DateField(auto_now=True)
+  para = models.TextField(max_length=1000)
+  image =models.ImageField(upload_to='images/')
